b5a6ee79aabd6e5840b858c50622ebd2b6c93934
Darren L. Weber, Ph.D <darren.weber@stanford.edu>
2015-03-26 08:46:46 -0700

    Revise configuration

--- lib/rdf-resource/configuration.rb
+++ lib/rdf-resource/configuration.rb
@@ -4,13 +4,38 @@ module RDFResource
   class Configuration
 
     attr_accessor :debug
+
+    attr_accessor :prefixes
+
+    attr_accessor :use_foaf
+    attr_accessor :use_schema
+
+    attr_accessor :log_file
     attr_accessor :logger
 
     def initialize
       @debug = env_boolean('DEBUG')
 
-      # logger
-      log_file = ENV['RDF_LOG_FILE'] || 'log/rdf_resource.log'
+      # Vocabulary options
+      # foaf:Person or schema:Person or both?
+      @use_foaf = env_boolean('USE_FOAF')
+      @use_schema = env_boolean('USE_SCHEMA') # schema.org
+
+      # RDF prefixes
+      @prefixes = {}
+      @prefixes['bf'] = 'http://bibframe.org/vocab/'
+      @prefixes['foaf'] = 'http://xmlns.com/foaf/0.1/'
+      @prefixes['isni'] = 'http://www.isni.org/isni/'
+      @prefixes['loc_names'] = 'http://id.loc.gov/authorities/names/'
+      @prefixes['loc_subjects'] = 'http://id.loc.gov/authorities/subjects/'
+      @prefixes['rdf'] = 'http://www.w3.org/1999/02/22-rdf-syntax-ns#'
+      @prefixes['rdfs'] = 'http://www.w3.org/2000/01/rdf-schema#'
+      @prefixes['schema'] = 'http://schema.org/'
+      @prefixes['owl'] = 'http://www.w3.org/2002/07/owl#'
+      @prefixes['viaf'] = 'http://viaf.org/viaf/'
+
+      # logging
+      log_file = ENV['RDF_LOG_FILE'] || 'log/rdf-resource.log'
       log_file = File.absolute_path log_file
       @log_file = log_file
       log_path = File.dirname log_file
@@ -19,13 +44,12 @@ module RDFResource
         Dir.mkdir log_path rescue nil
       end
       begin
-        log_dev = File.new(@log_file, 'w+')
+        log_file = File.new(@log_file, 'w+')
       rescue
-        log_dev = $stderr
+        log_file = $stderr
         @log_file = 'STDERR'
       end
-      log_dev.sync = true if @debug # skip IO buffering in debug mode
-      @logger = Logger.new(log_dev, 'monthly')
+      @logger = Logger.new(log_file, 'monthly')
       @logger.level = @debug ? Logger::DEBUG : Logger::INFO
 
     end
@@ -38,4 +62,3 @@ module RDFResource
   end
 
 end
-
--- lib/requires.rb
+++ lib/requires.rb
@@ -8,18 +8,18 @@ require 'pry-doc'
 require 'rest-client'
 RestClient.proxy = ENV['http_proxy'] unless ENV['http_proxy'].nil?
 RestClient.proxy = ENV['HTTP_PROXY'] unless ENV['HTTP_PROXY'].nil?
-if ENV['RACK_CACHE_ENABLED'].to_s.upcase == 'TRUE'
+if ENV['RDF_CACHE_ENABLED'].to_s.upcase == 'TRUE'
   require 'dalli'
   require 'restclient/components'
   require 'rack/cache'
-  # RestClient.enable Rack::CommonLogger
+  # TODO: use RDF_CACHE_LOG_FILE || STDOUT
   RestClient.enable Rack::CommonLogger, STDOUT
   # Enable the HTTP cache to store meta and entity data according
   # to the env config values or the defaults given here.  See
   # http://rtomayko.github.io/rack-cache/configuration for available options.
-  metastore = ENV['RACK_CACHE_METASTORE'] || 'file:/tmp/cache/meta'
-  entitystore = ENV['RACK_CACHE_ENTITYSTORE'] || 'file:/tmp/cache/body'
-  verbose = ENV['RACK_CACHE_VERBOSE'].to_s.upcase == 'TRUE' || false
+  metastore = ENV['RDF_CACHE_METASTORE'] || 'file:/tmp/cache/rdf_meta'
+  entitystore = ENV['RDF_CACHE_ENTITYSTORE'] || 'file:/tmp/cache/rdf_body'
+  verbose = ENV['RDF_CACHE_VERBOSE'].to_s.upcase == 'TRUE' || false
   RestClient.enable Rack::Cache,
     :metastore => metastore, :entitystore => entitystore, :verbose => verbose
 end
