b4a005fc3cef60c44a1b2a0347b8bd75927d01ef
Darren L. Weber, Ph.D <dweber.consulting@gmail.com>
2017-12-01 09:28:40 -0800

    random specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,6 +16,10 @@ SimpleCov.start 'rdf_resource'
 require 'rdf-resource'
 require 'rspec'
 RSpec.configure do |config|
-
+  # Run specs in random order to surface order dependencies. If you find an
+  # order dependency and want to debug it, you can fix the order by providing
+  # the seed, which is printed after each run.
+  #     --seed 1234
+  config.order = 'random'
 end
 
